886f38e3cb64919e07e9de911c51cf02b1902435
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-16 11:08:21 -0700

    implemented routes to create usernames and display them on users page

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("hi from middleware!");
-  next();
+const users = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/create-user", (req, res) => {
+  const username = req.body.username;
+  users.push(username);
+  res.redirect(302, "/users");
 });
 
-app.use((req, res, next) => {
-  console.log("hi from another middleware!");
-  res.send("<h1>Hello from Express!</h1>");
+app.use("/users", (req, res, next) => {
+  let output = "";
+  users.forEach(
+    (username, index) => (output += `<li>User ${index + 1}: ${username}</li>`)
+  );
+  res.send(`<ul>${output}</ul>`);
 });
 
-const server = http.createServer(app);
+app.use("/", (req, res, next) => {
+  res.send(`
+    <form action='/create-user' method='POST'>
+      <input type='text' name='username'/>
+      <button type='submit'>Submit</button>
+    </form>
+  `);
+});
 
-server.listen(3000);
+app.listen(3000);
