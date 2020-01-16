d23dc872ddb4e1e89b4f8e81a11849df7e937cdc
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-16 10:33:26 -0700

    added basic middleware

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const http = require("http");
 const express = require("express");
 
 const app = express();
+
+app.use((req, res, next) => {
+  console.log("hi from middleware!");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("hi from another middleware!");
+  res.send("<h1>Hello from Express!</h1>");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
